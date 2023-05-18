00df1b3efbfde3cdf712dd90ca1dc2cc2a1c605f
Agung Tri Utomo <agungtriu@gmail.com>
2023-05-18 22:14:41 +0700

    add feature filter by date homeCMS

--- client/src/axios/homeAxios.js
+++ client/src/axios/homeAxios.js
@@ -5,11 +5,11 @@ const baseUrl = config.baseUrl;
 
 const URL = baseUrl;
 
-const getHomeData = async (cb) => {
+const getHomeData = async (startDate, endDate, cb) => {
   try {
     let results = await axios({
       method: "GET",
-      url: URL + "/cms",
+      url: URL + "/cms?startDate=" + startDate + "&endDate=" + endDate,
       headers: {
         access_token: localStorage.access_token,
       },
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getHomeData } from "../../axios/homeAxios";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { DateRangePicker } from "react-date-range";
+import { addDays, subDays } from "date-fns";
+import { formatDate, readableDate } from "../../helpers/TimeFormat";
 
 const Dashboard = () => {
   const [clicked, setClicked] = useState(false);
@@ -9,104 +12,143 @@ const Dashboard = () => {
   const handleClick = () => {
     setClicked((clicked) => !clicked);
   };
+  const navigate = useNavigate();
+  const clickHandler = (link) => {
+    navigate(link);
+  };
+  useEffect(() => {
+    getHomeData(
+      formatDate(range[0].startDate),
+      formatDate(addDays(range[0].endDate, 1)),
+      (result) => {
+        setDashboard(result);
+      }
+    );
+  }, []);
+
+  const [range, setRange] = useState([
+    {
+      startDate: subDays(new Date(), 7),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
+  const [open, setOpen] = useState(false);
+
+  const refOne = useRef(null);
 
   useEffect(() => {
-    getHomeData((result) => {
-      setDashboard(result);
-    });
+    document.addEventListener("keydown", hideOnEscape, true);
+    document.addEventListener("click", hideOnClickOutside, true);
   }, []);
+
+  const hideOnEscape = (e) => {
+    if (e.key === "Escape") {
+      setOpen(false);
+    }
+  };
+
+  const hideOnClickOutside = (e) => {
+    if (refOne.current && !refOne.current.contains(e.target)) {
+      setOpen(false);
+    }
+  };
+  const changeHandler = (item) => {
+    getHomeData(
+      formatDate(addDays(item.startDate, 1)),
+      formatDate(addDays(item.endDate, 2)),
+      (result) => {
+        setDashboard(result);
+      }
+    );
+  };
   return (
     <>
-      <div className="row row-cols-4">
-        <div className="p-2">
-          <div
-            className="card  text-white"
-            style={{
-              maxWidth: "250px",
-              backgroundColor: "#29CC39",
-              borderColor: "#FFFF",
-            }}
-          >
-            <div className="card-body">
-              <h6 className="text-left">All Product</h6>
-              <h4 className="text-center my-4">{dashboard.countProduct}</h4>
+      <div className="row">
+        <h5 className="m-3 col">Dashboard</h5>
+        <div className="m-3 col text-end">
+          <div className="calendarWrap">
+            <button
+              className="btn btn-light dropdown-toggle"
+              onClick={() => setOpen((open) => !open)}
+            >
+              {`${readableDate(range[0].startDate)} - ${readableDate(
+                range[0].endDate
+              )}`}
+            </button>
+
+            <div ref={refOne}>
+              {open && (
+                <DateRangePicker
+                  onChange={(item) => {
+                    changeHandler(item.selection);
+                    setRange([item.selection]);
+                  }}
+                  editableDateInputs={true}
+                  moveRangeOnFirstSelection={false}
+                  ranges={range}
+                  months={2}
+                  direction="horizontal"
+                  className="calendarElement"
+                />
+              )}
             </div>
           </div>
         </div>
-        <Link className="p-2 text-decoration-none" to={"orders/new"}>
-          <div
-            className="card text-white"
-            style={{
-              maxWidth: "250px",
-              backgroundColor: "#8833FF",
-              borderColor: "#FFFF",
-            }}
-          >
-            <div className="card-body">
-              <h6 className="text-left">New Order</h6>
-              <h4 className="text-center my-4">{dashboard.countNewOrder}</h4>
-            </div>
-          </div>
-        </Link>
-        <div className="p-2">
-          <div
-            className="card  text-white"
-            style={{
-              maxWidth: "250px",
-              backgroundColor: "#FF6633",
-              borderColor: "#FFFF",
-            }}
-          >
-            <div className="card-body">
-              <h6 className="text-left">Profile Seen</h6>
-              <h4 className="text-center my-4">
-                {dashboard.countVisitAccount}
-              </h4>
-            </div>
-          </div>
+      </div>
+      <div className="row">
+        <div
+          className="col btn btn-lg btn-primary ms-3 p-4"
+          onClick={() => clickHandler("/products")}
+        >
+          <h6 className="text-start">All Product</h6>
+          <h4 className="text-center my-4">{dashboard.countProduct}</h4>
         </div>
-        <Link className="p-2 text-decoration-none" onClick={handleClick}>
+        <div
+          className="col btn btn-lg btn-warning ms-3 p-4 text-white"
+          onClick={() => clickHandler("/orders/new")}
+        >
+          <h6 className="text-start">New Order</h6>
+          <h4 className="text-center my-4">{dashboard.countNewOrder}</h4>
+        </div>
+        <div className="col btn btn-lg btn-info ms-3 p-4 text-white">
+          <div className="row">
+            <h6 className="text-start col">Profile Seen</h6>
+          </div>
+          <h4 className="text-center my-4">{dashboard.countVisitAccount}</h4>
+        </div>
+        <div
+          className="col btn btn-lg btn-success mx-3 p-4"
+          onClick={handleClick}
+        >
           {clicked ? true : false}
-          <div
-            className="card  text-white"
-            style={{
-              maxWidth: "250px",
-              backgroundColor: "#33BFFF",
-              borderColor: "#FFFF",
-            }}
-          >
-            <div className="card-body">
-              <h6 className="text-left">Product Seen</h6>
-              <h4 className="text-center my-4">
-                {dashboard.countVisitProduct}
-              </h4>
-            </div>
+          <div className="row">
+            <h6 className="text-start col">Product Seen</h6>
           </div>
-        </Link>
+          <h4 className="text-center my-4">{dashboard.countVisitProduct}</h4>
+        </div>
       </div>
       {clicked === true ? (
         <>
-          <div className="my-2">ListProduct</div>
-          <>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">No</th>
-                  <th scope="col">Product</th>
-                  <th scope="col">Seen</th>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">Product</th>
+                <th scope="col">Seen</th>
+              </tr>
+            </thead>
+            <tbody className="table-group-divider">
+              {dashboard.dataVisitProduct.map((item, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{item.name}</td>
+                  <td>{item.countVisit}</td>
                 </tr>
-              </thead>
-              <tbody className="table-group-divider">
-                {dashboard.dataVisitProduct.map((item, index) => (
-                  <tr key={index}>
-                    <th scope="row">{index + 1}</th>
-                    <td>{item.name}</td>
-                    <td>{item.countVisit}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </>
+              ))}
+            </tbody>
+          </table>
         </>
       ) : null}
     </>
