813a576161deef9d1321657a1b189ce41c149ffb
Agung Tri Utomo <agungtriu@gmail.com>
2023-05-18 23:10:20 +0700

    loading at homecms

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -4,24 +4,28 @@ import { useNavigate } from "react-router-dom";
 import { DateRange } from "react-date-range";
 import { addDays, subDays } from "date-fns";
 import { formatDate, readableDate } from "../../helpers/TimeFormat";
+import ReactLoading from "react-loading";
 
 const Dashboard = () => {
   const [clicked, setClicked] = useState(false);
   const [dashboard, setDashboard] = useState([]);
+  const [done, setDone] = useState(false);
 
-  const handleClick = () => {
-    setClicked((clicked) => !clicked);
+  const handleClick = (clicked) => {
+    setClicked(!clicked);
   };
   const navigate = useNavigate();
   const clickHandler = (link) => {
     navigate(link);
   };
   useEffect(() => {
+    setDone(false);
     getHomeData(
       formatDate(range[0].startDate),
       formatDate(addDays(range[0].endDate, 1)),
       (result) => {
         setDashboard(result);
+        setDone(true);
       }
     );
   }, []);
@@ -55,11 +59,13 @@ const Dashboard = () => {
     }
   };
   const changeHandler = (item) => {
+    setDone(false);
     getHomeData(
       formatDate(addDays(item.startDate, 1)),
       formatDate(addDays(item.endDate, 2)),
       (result) => {
         setDashboard(result);
+        setDone(true);
       }
     );
   };
@@ -101,52 +107,104 @@ const Dashboard = () => {
           onClick={() => clickHandler("/products")}
         >
           <h6 className="text-start">All Product</h6>
-          <h4 className="text-center my-4">{dashboard.countProduct}</h4>
+          <h4 className="text-center my-4">
+            {!done ? (
+              <ReactLoading
+                className="mx-auto"
+                type={"spin"}
+                color={"#000000"}
+                height={30}
+                width={30}
+              />
+            ) : (
+              dashboard.countProduct
+            )}
+          </h4>
         </div>
         <div
           className="col btn btn-lg btn-warning ms-3 p-4 text-white"
           onClick={() => clickHandler("/orders/new")}
         >
           <h6 className="text-start">New Order</h6>
-          <h4 className="text-center my-4">{dashboard.countNewOrder}</h4>
+          <h4 className="text-center my-4">
+            {!done ? (
+              <ReactLoading
+                className="mx-auto"
+                type={"spin"}
+                color={"#000000"}
+                height={30}
+                width={30}
+              />
+            ) : (
+              dashboard.countNewOrder
+            )}
+          </h4>
         </div>
-        <div className="col btn btn-lg btn-info ms-3 p-4 text-white">
+        <div
+          className="col btn btn-lg btn-info ms-3 p-4 text-white"
+          onClick={() => handleClick(true)}
+        >
           <div className="row">
             <h6 className="text-start col">Profile Seen</h6>
           </div>
-          <h4 className="text-center my-4">{dashboard.countVisitAccount}</h4>
+          <h4 className="text-center my-4">
+            {!done ? (
+              <ReactLoading
+                className="mx-auto"
+                type={"spin"}
+                color={"#000000"}
+                height={30}
+                width={30}
+              />
+            ) : (
+              dashboard.countVisitAccount
+            )}
+          </h4>
         </div>
         <div
           className="col btn btn-lg btn-success mx-3 p-4"
-          onClick={handleClick}
+          onClick={() => handleClick(clicked)}
         >
-          {clicked ? true : false}
           <div className="row">
             <h6 className="text-start col">Product Seen</h6>
           </div>
-          <h4 className="text-center my-4">{dashboard.countVisitProduct}</h4>
+          <h4 className="text-center my-4">
+            {!done ? (
+              <ReactLoading
+                className="mx-auto"
+                type={"spin"}
+                color={"#000000"}
+                height={30}
+                width={30}
+              />
+            ) : (
+              dashboard.countVisitProduct
+            )}
+          </h4>
         </div>
       </div>
       {clicked === true ? (
         <>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">No</th>
-                <th scope="col">Product</th>
-                <th scope="col">Seen</th>
-              </tr>
-            </thead>
-            <tbody className="table-group-divider">
-              {dashboard.dataVisitProduct.map((item, index) => (
-                <tr key={index}>
-                  <th scope="row">{index + 1}</th>
-                  <td>{item.name}</td>
-                  <td>{item.countVisit}</td>
+          {dashboard.countVisitProduct > 0 ? (
+            <table className="table table-hover my-3">
+              <thead>
+                <tr className="table-active">
+                  <th scope="col">No</th>
+                  <th scope="col">Product</th>
+                  <th scope="col">Seen</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {dashboard.dataVisitProduct.map((item, index) => (
+                  <tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{item.name}</td>
+                    <td>{item.countVisit}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : null}
         </>
       ) : null}
     </>
