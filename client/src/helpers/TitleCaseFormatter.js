d084780b05d600752ce58d022c08d2559640a4e4
Agung Tri Utomo <agungtriu@gmail.com>
2023-05-24 16:13:40 +0700

    bug fix TitleCaseFormatter

--- client/src/helpers/TitleCaseFormatter.js
+++ client/src/helpers/TitleCaseFormatter.js
@@ -2,7 +2,11 @@ const TitleCaseFormatter = (text) => {
   return text !== undefined
     ? text
         .split(" ")
-        .map((text) => text[0].toUpperCase() + text.slice(1).toLowerCase())
+        .map((text) =>
+          text[0] !== "" && text[0] !== null && text[0] !== undefined
+            ? text[0].toUpperCase() + text.slice(1).toLowerCase()
+            : null
+        )
         .join(" ")
     : text;
 };
